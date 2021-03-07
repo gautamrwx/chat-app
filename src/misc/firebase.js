8910848f40f3187d7c5607e4e1df392aa71a1c8e
gautamrwx <gautam.404@protonmail.com>
2021-03-07 17:48:32 +0530

    Firebase basic configuration done

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBF9XnRRBJIAPx7TrcGrEAI3W8RL_Isy8E',
+  authDomain: 'chat-web-app-ac1dc.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-ac1dc-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-ac1dc',
+  storageBucket: 'chat-web-app-ac1dc.appspot.com',
+  messagingSenderId: '144563113151',
+  appId: '1:144563113151:web:e291b15e79be0b182f4a0a',
+};
+
+const app = firebase.initializeApp(config);
