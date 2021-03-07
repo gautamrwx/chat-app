6020a4f189bb1e7532277cbfc7d10a6172fbb373
gautamrwx <gautam.404@protonmail.com>
2021-03-07 19:31:47 +0530

    Added feature to login with google.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <Switch>
+      <Route path="/signin">
+        <SignIn />
+      </Route>
+      <PrivateRoute path="/home">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Container, Grid, Row, Col, Panel, Button, Icon, Alert } from 'rsuite';
+import firebase from 'firebase/app';
+import { auth, database } from '../misc/firebase';
+
+const SignIn = () => {
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed In', 4000);
+    } catch (err) {
+      Alert.info(err.message, 4000);
+    }
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container className="mt-page">
+      <Grid>
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome To Chat</h2>
+                <p>Progressive Chat App</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="blue" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue With Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
+};
+
+export default SignIn;
