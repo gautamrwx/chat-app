4e58f512a91eba1bedee0a1de181592ad454d89c
gautamrwx <gautam.404@protonmail.com>
2021-03-09 00:17:17 +0530

    Avatar upload and display is ok..

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,17 +1,63 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import AvatarEditor from 'react-avatar-editor';
 import { Alert, Button, Modal } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 import { useModalState } from '../../misc/custom-hooks';
+import { database, storage } from '../../misc/firebase';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 
 const acceptedFileTypes = ['image/png', 'image/png', 'image/jpeg'];
 const isValidFile = file => acceptedFileTypes.includes(file.type);
 
+const getBlob = canvas => {
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(blob => {
+      if (blob) {
+        resolve(blob);
+      } else {
+        reject(new Error('File Process Error'));
+      }
+    });
+  });
+};
+
 const AvatarUploadBtn = () => {
   const { isOpen, open, close } = useModalState();
+  const [isLoading, setisLoading] = useState(false);
+  const { profile } = useProfile();
   const [img, setimg] = useState(null);
+  const avatarEditorRef = useRef();
+
+  const onUploadClick = async () => {
+    setisLoading(true);
+    const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+
+    try {
+      const blob = await getBlob(canvas);
+      const avatarRef = storage.ref(`/profile/${profile.uid}`).child('avatar');
+
+      const uploadAvatarResult = await avatarRef.put(blob, {
+        cacheControl: `public, max-age-${3600 * 24 * 3}`,
+      });
+
+      const downloadURL = await uploadAvatarResult.ref.getDownloadURL();
+
+      const userAvatarRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+
+      userAvatarRef.set(downloadURL);
+
+      Alert.success('Avatar has been Uploaded', 4000);
+      setisLoading(false);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+      setisLoading(false);
+    }
+  };
 
   const onFileInputChange = ev => {
     const currFile = ev.target.files;
@@ -29,6 +75,12 @@ const AvatarUploadBtn = () => {
 
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
+
       <div>
         <label
           htmlFor="avatar-upload"
@@ -52,6 +104,7 @@ const AvatarUploadBtn = () => {
             <div className="d-flex justify-content-center align-items-center h-100">
               {img && (
                 <AvatarEditor
+                  ref={avatarEditorRef}
                   image={img}
                   width={250}
                   height={250}
@@ -63,7 +116,12 @@ const AvatarUploadBtn = () => {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button appearance="ghost" block>
+            <Button
+              appearance="ghost"
+              block
+              onClick={onUploadClick}
+              disabled={isLoading}
+            >
               Upload New Avatar
             </Button>
           </Modal.Footer>
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -14,11 +14,12 @@ export const ProfileProvider = ({ children }) => {
       if (authObj) {
         useRef = database.ref(`/profiles/${authObj.uid}`);
         useRef.on('value', snap => {
-          const { name, createdAt } = snap.val();
+          const { name, createdAt, avatar } = snap.val();
 
           const data = {
             name,
             createdAt,
+            avatar,
             uid: authObj.uid,
             email: authObj.email,
           };
