cf5a2fa5cc850371bfbce14ee6e9b58377f7ff21
gautamrwx <gautam.404@protonmail.com>
2021-03-10 04:06:18 +0530

    Added user presense color dot for online or offline ..

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,16 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const profileContext = createContext();
 
@@ -9,9 +20,11 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let useRef;
+    let userStatusRef;
 
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         useRef = database.ref(`/profiles/${authObj.uid}`);
         useRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
@@ -27,10 +40,29 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setisLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (!!snapshot.val() === false) {
+            return;
+          }
+
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (useRef) {
           useRef.off();
         }
+
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+
+        database.ref('.info/connected').off();
 
         setProfile(null);
         setisLoading(false);
