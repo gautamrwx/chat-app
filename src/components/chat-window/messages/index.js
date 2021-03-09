3c48b5f4af20fc88b4cbf2ceef2947c5311d5356
gautamrwx <gautam.404@protonmail.com>
2021-03-09 23:57:17 +0530

    Now we can chat in this appp....

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrWithId } from '../../../misc/helper';
+import MessageItem from './MessageItem';
 
 const ChatMessages = () => {
-  return <div>cmessage</div>;
+  const { chatId } = useParams();
+  const [messages, setmessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessage = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messageRef = database.ref('/messages');
+
+    messageRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrWithId(snap.val());
+        setmessages(data);
+      });
+
+    return () => {
+      messageRef.off('value');
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No Messages Found</li>}
+      {canShowMessage &&
+        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
 };
 
 export default ChatMessages;
