7f0bcbc4de33e9a6b16a1a30f2d096a94d3c6a80
gautamrwx <gautam.404@protonmail.com>
2021-03-09 20:47:08 +0530

    Displayed Last message in sidebar with sender name

--- src/components/rooms/ChatRoomItem.js
+++ src/components/rooms/ChatRoomItem.js
@@ -1,21 +1,46 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Avatar } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../ProfileAvatar';
 
 const ChatRoomItem = ({ room }) => {
-  const { name, createdAt, description } = room;
+  const { name, createdAt, lastMessage } = room;
 
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">{name}</h3>
         <span>
-          <TimeAgo datetime={new Date(createdAt)} locale="en_IN" />
+          <TimeAgo
+            datetime={
+              lastMessage
+                ? new Date(lastMessage.createdAt)
+                : new Date(createdAt)
+            }
+            locale="en_IN"
+          />
         </span>
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        <span>{description}</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+            <div className="text-disappear ml-2">
+              <div className="italic"> {lastMessage.author.name}</div>
+              <span>{lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No Message Yet</span>
+        )}
       </div>
     </div>
   );
