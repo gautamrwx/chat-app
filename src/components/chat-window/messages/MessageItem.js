307890b2aefc7cc87246a5b257ed191cda520ff6
gautamrwx <gautam.404@protonmail.com>
2021-03-11 00:10:34 +0530

    Admin Add/remove Button Created on vieewProfile popup

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 import PresenseDot from '../../PresenseDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
     <li className="padded mb-1">
@@ -22,7 +32,16 @@ const MessageItem = ({ message }) => {
           profile={author}
           appearance="link"
           className="ml-1 p-0 text-black"
-        />
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove admin permission'
+                : 'Give admin in this room'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
+
         <TimeAgo
           datetime={createdAt}
           locale="en_IN"
@@ -37,4 +56,4 @@ const MessageItem = ({ message }) => {
   );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
--- src/components/chat-window/messages/ProfileInfoBtnModal.js
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -3,7 +3,7 @@ import { Button, Modal } from 'rsuite';
 import { useModalState } from '../../../misc/custom-hooks';
 import ProfileAvatar from '../../ProfileAvatar';
 
-const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
+const ProfileInfoBtnModal = ({ profile, children, ...btnProps }) => {
   const { isOpen, close, open } = useModalState();
 
   const shortName = profile.name.split(' ')[0];
@@ -30,6 +30,7 @@ const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
           <p>Memeber Since {memberSince}</p>
         </Modal.Body>
         <Modal.Footer>
+          {children}
           <Button block onClick={close}>
             Close
           </Button>
